1917f1d2b37edf8bc4a8ab4a5b5e2aa35daef08f
joe34674 <joepaul.martin1448@gmail.com>
2024-02-24 12:58:20 +0530

    updates

--- cmd/chat-client/main.go
+++ cmd/chat-client/main.go
@@ -1,5 +1,60 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
 func main() {
+
+	u := url.URL{
+		Scheme: "ws",
+		Host:   "127.0.0.1:8080",
+		Path:   "/ws",
+	}
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sender(conn)
+
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Message from server : %v\n", string(msg))
+
+	fmt.Println("Exiting the application")
+}
+
+func sender(conn *websocket.Conn) {
+
+	// Create a new scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Read input line by line
+	for scanner.Scan() {
+		text := scanner.Text() // Get the current line of text
+		if text == "" {
+			break // Exit loop if an empty line is entered
+		}
+		err := conn.WriteMessage(websocket.TextMessage, []byte(text))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error from scanner:", err)
+	}
 
 }
